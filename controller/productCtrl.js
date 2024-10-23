76c292297891104dee109c33258c3bf629db7c1b
shakir raza <shakirraza737@gmail.com>
2024-10-23 17:08:32 +0300

    initial

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -6,54 +6,50 @@ const validateMongoDbId = require("../utils/validateMongodbId");
 const cloudinary = require("cloudinary").v2;
 const path = require("path");
 
-const createProduct = asyncHandler(async(req, res) => {
-  try{
-    
-      if(req.body.title) {
-          const newProduct = req.body;
-          console.log(newProduct);
-          newProduct.slug = slugify(req.body.title);
-      
-          const already = await Product.findOne({ slug: newProduct.slug});
-        
-            if(already){
-                 res.status(409).send({
-                 success: false,           
-                 message: "Product Already Exist",
-                 });
-       } else {
-       
-    
-      const product = await Product.create(newProduct);
-      res.json(product);
-        }
-      }else{
-        res.status(500).send({
+const createProduct = asyncHandler(async (req, res) => {
+  try {
+    if (req.body.title) {
+      const newProduct = req.body;
+      console.log(newProduct);
+      newProduct.slug = slugify(req.body.title);
+
+      const already = await Product.findOne({ slug: newProduct.slug });
+
+      if (already) {
+        res.status(409).send({
           success: false,
-          error,
-          message: "One of required field is empty",
-         });
+          message: "Product Already Exist",
+        });
+      } else {
+        const product = await Product.create(newProduct);
+        res.json(product);
       }
-  } catch (error){
-      throw new Error(error);
+    } else {
+      res.status(500).send({
+        success: false,
+        error,
+        message: "One of required field is empty",
+      });
+    }
+  } catch (error) {
+    throw new Error(error);
   }
 });
 
 const updateProduct = asyncHandler(async (req, res) => {
-   
-   const id = req.body.id;
+  const id = req.body.id;
   // validateMongoDbId(id);
   try {
-     if (req.body.title) {
-       req.body.slug = slugify(req.body.title);
-     }
-    
-     const updateProduct = await Product.findByIdAndUpdate(id , req.body, {
-       new: true,
-     });
-    
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title);
+    }
+
+    const updateProduct = await Product.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+
     const productList = await Product.find();
-  
+
     res.json(productList);
   } catch (error) {
     throw new Error(error);
@@ -86,44 +82,44 @@ const getAllProduct = asyncHandler(async (req, res) => {
   try {
     // Filtering
 
-    const queryObj = { ...req.query };
-    const excludeFields = ["page", "sort", "limit", "fields"];
-    excludeFields.forEach((el) => delete queryObj[el]);
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    // const queryObj = { ...req.query };
+    // const excludeFields = ["page", "sort", "limit", "fields"];
+    // excludeFields.forEach((el) => delete queryObj[el]);
+    // let queryStr = JSON.stringify(queryObj);
+    // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    let query = Product.find(JSON.parse(queryStr));
+    let query = await Product.find();
 
     // Sorting
-    
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" ");
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort("-createdAt");
-    }
+
+    // if (req.query.sort) {
+    //   const sortBy = req.query.sort.split(",").join(" ");
+    //   query = query.sort(sortBy);
+    // } else {
+    //   query = query.sort("-createdAt");
+    // }
 
     // limiting the fields
 
-    if (req.query.fields) {
-      const fields = req.query.fields.split(",").join(" ");
-      query = query.select(fields);
-    } else {
-      query = query.select("-__v");
-    }
+    // if (req.query.fields) {
+    //   const fields = req.query.fields.split(",").join(" ");
+    //   query = query.select(fields);
+    // } else {
+    //   query = query.select("-__v");
+    // }
 
     // pagination
 
-    const page = req.query.page;
-    const limit = req.query.limit;
-    const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit);
-    if (req.query.page) {
-      const productCount = await Product.countDocuments();
-      if (skip >= productCount) throw new Error("This Page does not exists");
-    }
+    // const page = req.query.page;
+    // const limit = req.query.limit;
+    // const skip = (page - 1) * limit;
+    // query = query.skip(skip).limit(limit);
+    // if (req.query.page) {
+    //   const productCount = await Product.countDocuments();
+    //   if (skip >= productCount) throw new Error("This Page does not exists");
+    // }
 
-    const product = await query;
+    const product = query;
     res.json(product);
   } catch (error) {
     throw new Error(error);
